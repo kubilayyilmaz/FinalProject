330fc81f673959f7e1481b85528e36bc81fac0b9
Kubilay Yılmaz <kubilayyilmaz@outlook.com.tr>
2023-10-18 00:33:59 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T> 
+        //Her Dal sınıfında nesnemizin ismi ile methodlar oluşturmak yerine Generic yapı kullanabiliriz, bu repository sınıfda hepsinde olan methodlar yer alır.
+    {
+        List<T> GetAll(Expression<Func<T, bool>>? filter = null); // p=>p.CategoryId == 2 şeklinde koşul yazabilmemezi sağlayan delegate
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    //I -> Onun interface olduğunu anlatır,
+    //Product -> Hangi tabloya karşılık geldiğini anlatır,
+    //Dal (Data Access Layer) veya Dao (Data Access Object) -> Onun hangi katmana karşılık geldiğini anlatır.
+
+    public interface IProductDal : IEntityRepository<Product>
+    {
+
+    }
+}
