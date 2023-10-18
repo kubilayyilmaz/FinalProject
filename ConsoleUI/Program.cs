9c06a5b549050c59736882281f4a03e16ea4b4b3
Kubilay Yılmaz <kubilayyilmaz@outlook.com.tr>
2023-10-19 00:27:13 +0300

    19.10.2023

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,15 +1,20 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 
 namespace ConsoleUI
 {
     internal class Program
     {
+        //SOLID
+        //Open Closed Principle
+        //Yeni bir özellik ekliyor isek mevcuttaki hiçbir koduna dokunamazsın.
+
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAll())
+            foreach (var product in productManager.GetAllByCategoryId(2))
             {
                 Console.WriteLine(product.ProductName);
             }
--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -7,8 +8,13 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface IEntityRepository<T> 
-        //Her Dal sınıfında nesnemizin ismi ile methodlar oluşturmak yerine Generic yapı kullanabiliriz, bu repository sınıfda hepsinde olan methodlar yer alır.
+    //Her Dal sınıfında nesnemizin ismi ile methodlar oluşturmak yerine Generic yapı kullanabiliriz, bu repository sınıfda hepsinde olan methodlar yer alır.
+    
+    //IEntityRepository implemente eden nesnelerimizde vereceğimiz generic nesneyi sadece IEntity (kendisi) ve implemente eden nesneleri kullansın şeklinde kısıtlayabiliriz. Buna Generic Constraint denir.
+    //Class : Referans Tip
+    //IEntity: IEntity olabilir veya IEntity implemente eden bir nesne olabilir.
+    //new() : new'lenebilir olmalı
+    public interface IEntityRepository<T> where T : class, IEntity, new()
     {
         List<T> GetAll(Expression<Func<T, bool>>? filter = null); // p=>p.CategoryId == 2 şeklinde koşul yazabilmemezi sağlayan delegate
         T Get(Expression<Func<T, bool>> filter);
